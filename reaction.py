88fb7ca910a0a3f9dfe13d26fbea279d22e2b10d
Xuanru Guo <2722458837@qq.com>
2025-04-09 14:32:04 +0100

    fourth commit reaction.py -- get players name

--- reaction.py
+++ reaction.py
@@ -1,7 +1,7 @@
-# Version 3
+# Version 4
 # Author: Xuanru Guo
 # Date: 9/4/2025
-# detect the buttons
+# get players name using input
 
 # import modules and libraries
 from gpiozero import LED, Button
@@ -13,15 +13,22 @@ led = LED(4)
 right_button = Button(15)
 left_button = Button(14)
 
+# get the players name
+left_name = input("left player name is ")
+right_name = input("right player name is ")
+
 # wait for 5 seconds and turn the LED on
 led.on()
 # the length of time is random
 sleep(uniform(5,10))
 led.off()
 
-# tell which pin the button is on
+# tell which player press the button
 def pressed(button):
-	print(str(button.pin.number) + ' won the game')
+	if button.pin.number == 14:
+		print(left_name + ' won the game')
+	else:
+		print(right_name + ' won the game')
 
 # when the button is pressed, the funtion is called
 right_button.when_pressed = pressed

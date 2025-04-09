fb922f8905c373133aa5f25d4b3f69ba2945afb5
Xuanru Guo <2722458837@qq.com>
2025-04-09 16:24:39 +0100

    seventh commit reaction.py -- add scores for players

--- reaction.py
+++ reaction.py
@@ -1,10 +1,10 @@
-# Version 6
+# Version 7
 # Author: Xuanru Guo
 # Date: 9/4/2025
-# use loop to play game again
-# use flag to stop pressing button when the LED is still on
+# add scores for both players
 
 # import modules and libraries
+import sys
 from gpiozero import LED, Button
 from time import sleep
 from random import uniform
@@ -18,16 +18,22 @@ left_button = Button(14)
 left_name = input("left player name is ")
 right_name = input("right player name is ")
 
+# initialize players scores
+left_score = 0
+right_score = 0
+
 can_press = False
 
 # tell which player press the button
 def pressed(button):
-    global can_press
+    global can_press, left_score, right_score
     if can_press:
         if button.pin.number == 14:
             print(left_name + ' won the game')
+            left_score += 1
         else:
             print(right_name + ' won the game')
+            right_score += 1
         can_press = False
 
 
@@ -46,7 +52,12 @@ try:
         # rest time before next game
         sleep(1.5)
 except KeyboardInterrupt:
+    sys.stdout.write("\r")  
+    sys.stdout.flush()  
     print("Game over")
+    print(f"{left_name}'s total score: {left_score}")
+    print(f"{right_name}'s total score: {right_score}")
+finally:
     led.close()
     right_button.close()
     left_button.close()

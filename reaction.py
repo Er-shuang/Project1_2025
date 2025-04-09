334d20bd302b5cb8a111ae5a23c611e4ec67d794
Xuanru Guo <2722458837@qq.com>
2025-04-09 02:41:48 +0100

    second commit reaction.py -- add an element of surprise

--- reaction.py
+++ reaction.py
@@ -1,16 +1,19 @@
-# Version 1
+# Version 2
 # Author: Xuanru Guo
+# Add an element of surprise
 
 # import modules and libraries
 from gpiozero import LED, Button
 from time import sleep
+from random import uniform
 
 # set up the pin
 led = LED(4)
 
 # wait for 5 seconds and turn the LED on
 led.on()
-sleep(5)
+# the length of time is random
+sleep(uniform(5,10))
 led.off()
 
 

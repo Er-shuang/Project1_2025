8ee4d3d7bfea814eb1dff20b045712f8518e693e
Er-shuang <1343109389@qq.com>
2025-04-10 23:22:17 +0800

    Get Player names, tell which player wins

--- reaction.py
+++ reaction.py
@@ -1,7 +1,7 @@
 # Author: Nie Yujin
 # Date: 10/4/25
 # A reaction game with LED
-# version 3: detect the buttons
+# version 4: get player names
 
 from gpiozero import LED,Button
 from time import sleep
@@ -12,6 +12,10 @@ led=LED(4)
 right_button=Button(15)
 left_button=Button(14)
 
+# input the name of player
+left_name=input('left player name is ')
+right_name=input('right player name is ')
+
 # turn on the LED
 led.on()
 # light up the LED for random time(5~10 secomds)
@@ -19,9 +23,13 @@ sleep(uniform(5,10))
 # turn off the LED
 led.off()
 
-# a function tell which pin the button was on
+# a function tell which pin the button was on, and which player win the game
 def pressed(button):
-    print(str(button.pin.number) + ' won the game')
+    if button.pin.number==14:
+        print(left_name+' won the game')
+    else:
+        print(right_name+' won the game')
+    exit()
 
 # press button and call the pressed function
 right_button.when_pressed=pressed

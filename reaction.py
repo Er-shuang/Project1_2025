64539f51dd74676e9fd87d37afdf7b1242e77b20
Er-shuang <1343109389@qq.com>
2025-04-10 22:32:29 +0800

    2. Add an element of surprise, light up for 5~10 seconds

--- reaction.py
+++ reaction.py
@@ -1,17 +1,18 @@
 # Author: Nie Yujin
 # Date: 10/4/25
 # A reaction game with LED
-# version 1: turn on and off the LED for 5 second
+# version 2: light up the LED for random time(5~10seconds)
 
 from gpiozero import LED,Button
 from time import sleep
+from random import uniform
 
 # name the pin
 led=LED(4)
 # turn on the LED
 led.on()
-# light on the LED for 5 second
-sleep(5)
+# light up the LED for random time(5~10 secomds)
+sleep(uniform(5,10))
 # turn off the LED
 led.off()
 

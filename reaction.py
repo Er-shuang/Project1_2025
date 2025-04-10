b58a054bab1d899420f98fd788c3b4866e271cdc
Er-shuang <1343109389@qq.com>
2025-04-10 23:12:47 +0800

    Detect the buttons, record which button win

--- reaction.py
+++ reaction.py
@@ -1,14 +1,17 @@
 # Author: Nie Yujin
 # Date: 10/4/25
 # A reaction game with LED
-# version 2: light up the LED for random time(5~10seconds)
+# version 3: detect the buttons
 
 from gpiozero import LED,Button
 from time import sleep
 from random import uniform
 
-# name the pin
+# name the pin for LED and buttons
 led=LED(4)
+right_button=Button(15)
+left_button=Button(14)
+
 # turn on the LED
 led.on()
 # light up the LED for random time(5~10 secomds)
@@ -16,3 +19,10 @@ sleep(uniform(5,10))
 # turn off the LED
 led.off()
 
+# a function tell which pin the button was on
+def pressed(button):
+    print(str(button.pin.number) + ' won the game')
+
+# press button and call the pressed function
+right_button.when_pressed=pressed
+left_button.when_pressed=pressed

5873b37402e88632d2f6f6cefa50d2becba16721
Er-shuang <1343109389@qq.com>
2025-04-11 14:38:01 +0800

    Add recording reaction time function

--- reaction.py
+++ reaction.py
@@ -1,16 +1,19 @@
 # Author: Nie Yujin
-# Date: 10/4/25
+# Date: 11/4/25
 # A reaction game with LED
-# version 4: get player names
+# version 5: record the time that player pressed button after LED turned off
 
 from gpiozero import LED,Button
-from time import sleep
+from time import sleep,time
 from random import uniform
 
 # name the pin for LED and buttons
 led=LED(4)
 right_button=Button(15)
 left_button=Button(14)
+
+# time of LED turn off
+off_time=0
 
 # input the name of player
 left_name=input('left player name is ')
@@ -22,13 +25,18 @@ led.on()
 sleep(uniform(5,10))
 # turn off the LED
 led.off()
+off_time=time()
 
 # a function tell which pin the button was on, and which player win the game
 def pressed(button):
     if button.pin.number==14:
-        print(left_name+' won the game')
+        # reaction time
+        reaction_time=time()-off_time
+        print(f"{left_name} won the game. Reaction time is {reaction_time}s")
     else:
-        print(right_name+' won the game')
+        # reaction time
+        reaction_time=time()-off_time
+        print(f"{right_name} won the game. Reaction time is {reaction_time}s")
     exit()
 
 # press button and call the pressed function
